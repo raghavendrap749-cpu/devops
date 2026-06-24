0cf1cb94ebfac3aaa0eebe47eaafe6f53537a6de
raghavendrap749-cpu <raghavendrap749@gmail.com>
2026-06-24 17:02:24 +0530

    add variable injection

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 resource local_file file{
-filename = "abc.txt"
-content = "terraform cloud with github"
+filename = var.file_name
+content = var.content
+}
+variable file_name{
+default = "123.txt"
+}
+variable content{
+default = "this is Raghavendra"
 }
